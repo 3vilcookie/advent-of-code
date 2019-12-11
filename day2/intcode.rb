6289f538b7550c087cf918236120864da82c6fa6
raphael.pour <raphael.pour@hetzner.com>
2019-12-11 13:07:54 +0100

    day2: Add memory dump function

--- day2/intcode.rb
+++ day2/intcode.rb
@@ -1,8 +1,14 @@
+
+def dump_memory(memory)
+  puts("Memory dump:\n")
+  p memory
+end
+
 program = STDIN.read.chomp.split(',').map(&:to_i)
 
-memory = {1 => 12, 2 => 2 }
+memory = {0 => 12, 1 => 2 }
 
-puts("Opcode | Op1 | Op2 | Addr |\n")
+puts("Op | Op1 | Op2 | Addr |\n")
 (0...program.size).step(4).each do |i|
   break if program[i] == 99
 
@@ -10,18 +16,18 @@ puts("Opcode | Op1 | Op2 | Addr |\n")
 
   puts("#{opcode} | #{op1} | #{op2} | #{addr}|\n") 
 
-  raise("Access violation") unless memory.key?(op1) and memory.key?(op2)
-  memory << addr unless memory?key(addr)
+  dump_memory(memory);raise("Access violation with op1") unless memory.key?(op1)
+  dump_memory(memory);raise("Access violation with op2") unless memory.key?(op2)
+
   case opcode
   when 1 
-    memory[addr] = memory[op1] + memory[op2]
+    memory[addr-1] = memory[op1] + memory[op2]
   when 2
-    memory[addr] = memory[op1] - memory[op2]
+    memory[addr-1] = memory[op1] * memory[op2]
   end
 
 end
 
 puts("End of program\n")
 
-puts("Memory dump:\n")
-p memory
+dump_memory
