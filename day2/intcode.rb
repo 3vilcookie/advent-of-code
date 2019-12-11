b5e3713092f7d17c63c55e1e181a5e220ec22197
Raphael Pour <info@raphaelpour.de>
2019-12-11 21:03:23 +0100

    day2: Finishes intcode interpreter

--- day2/intcode.rb
+++ day2/intcode.rb
@@ -1,33 +1,23 @@
-
-def dump_memory(memory)
-  puts("Memory dump:\n")
-  p memory
-end
-
 program = STDIN.read.chomp.split(',').map(&:to_i)
 
-memory = {0 => 12, 1 => 2 }
+# Patch program
+program[1] = 12
+program[2] = 2
 
-puts("Op | Op1 | Op2 | Addr |\n")
+puts("Op| Op1| Op2| Addr|\n")
 (0...program.size).step(4).each do |i|
   break if program[i] == 99
 
   opcode, op1, op2, addr = program[i,4]
 
-  puts("#{opcode} | #{op1} | #{op2} | #{addr}|\n") 
-
-  dump_memory(memory);raise("Access violation with op1") unless memory.key?(op1)
-  dump_memory(memory);raise("Access violation with op2") unless memory.key?(op2)
+  puts("#{opcode.to_s.rjust(2)}|#{op1.to_s.rjust(4)}|#{op2.to_s.rjust(4)}|#{addr.to_s.rjust(5)}|\n") 
 
   case opcode
   when 1 
-    memory[addr-1] = memory[op1] + memory[op2]
+    program[addr] = program[op1] + program[op2]
   when 2
-    memory[addr-1] = memory[op1] * memory[op2]
+    program[addr] = program[op1] * program[op2]
   end
 
 end
-
-puts("End of program\n")
-
-dump_memory
+puts("Value at Addr 0: " + program[0].to_s)
