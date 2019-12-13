4d9559781193bc285e8366bd3a87173c076fb570
raphael.pour <raphael.pour@hetzner.com>
2019-12-13 12:42:17 +0100

    day4: Solves part1

--- day4/crack.rb
+++ day4/crack.rb
@@ -1,5 +1,7 @@
-puts (372304...847060).to_a.inject(:+) do |pw|
-  next 0 unless pw.to_s.scan(/(.)\1/).size >= 2
-  next 0 unless pw.to_s.chars.inject(0){|p,d| break false unless p<=d;d}
+a =  (372304..847060).to_a.map do |pw|
+  next 0 if pw.to_s.scan(/(.)\1/).empty?
+  next 0 unless pw.to_s.chars.inject(0){|p,d| break false unless p.to_i<=d.to_i;d.to_i} != false
   1
-end
+end.sum
+
+puts a
